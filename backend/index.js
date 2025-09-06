4d35c1f81e68b7ec1e83840374211f9eebceebcd
luthfior <luthfiraama@gmail.com>
2025-09-06 18:50:39 +0700

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,15 @@ import { getFirestore } from "firebase-admin/firestore";
 
 dotenv.config();
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined;
+
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        privateKey,
     }),
 });
 
